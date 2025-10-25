4e930a3e80d9790e3ea9107601969b8d828b6492
cfasociety2025 <cfasocietyeastafrica@gmail.com>
2025-10-25 12:20:53 +0300

    added google analytics script

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,6 @@
 import "./globals.css";
 import { Analytics } from "@vercel/analytics/next";
+import Script from "next/script";
 
 export const metadata = {
   title: "Africa Investment Conference 2025 | AfIC 2025 | CFA Society East Africa",
@@ -32,6 +33,23 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className="antialiased">
+        {/* Google Analytics */}
+        <Script
+          strategy="afterInteractive"
+          src="https://www.googletagmanager.com/gtag/js?id=G-0JRFF4F8SP"
+        />
+        <Script
+          id="google-analytics"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-0JRFF4F8SP');
+            `,
+          }}
+        />
         {children}
         <Analytics />
       </body>
